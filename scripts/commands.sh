20c7385252d71e1e3efb50391255c745276ece87
Saymon Farias <saymonmaciel117@gmail.com>
2026-07-10 12:33:35 -0300

    Modificações necessárias para que o projeto utilize um container separado do postgresql

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -3,7 +3,7 @@
 # O shell irá encerrar a execução do script quando um comando falhar
 set -e
 
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+while ! nc -z -w 1 "$POSTGRES_HOST" "$POSTGRES_PORT"; do
   echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
   sleep 2
 done
